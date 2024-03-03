687dbc58227f00cd774b00e32a046a70206eab69
ZachK2026 <zk21@rice.edu>
2024-03-03 15:31:39 -0600

    package updated, switch to index.html

--- my-app/config-overrides.js
+++
@@ -1,7 +0,0 @@
-console.log('config-overrides.js is loaded')
-
-const { override, addBabelPlugin } = require('customize-cra')
-
-module.exports = override(
-    addBabelPlugin(['@babel/plugin-transform-react-jsx', { 'throwIfNamespace': false }])
-)
